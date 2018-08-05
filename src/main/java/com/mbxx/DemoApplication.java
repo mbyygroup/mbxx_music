93a9d20b0c5a6997f9b7f42f6fbbd0669a106a36
mbyy <17600733846@163.com>
2018-08-05 22:10:55 +0800

    日志部分暂时告一段落

--- src/main/java/com/mbxx/DemoApplication.java
+++ src/main/java/com/mbxx/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.mbxx;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.mbxx.dao")
 public class DemoApplication {
-
+    protected static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
+        logger.info("springboot开始");
         SpringApplication.run(DemoApplication.class, args);
+        logger.info("springboot结束");
     }
 }
--- src/main/java/com/mbxx/controller/UserController.java
+++ src/main/java/com/mbxx/controller/UserController.java
@@ -2,6 +2,8 @@ package com.mbxx.controller;
 
 import com.mbxx.pojo.User;
 import com.mbxx.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    private static Logger logger= LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @ResponseBody
     @PostMapping("/add")
     public int addUser(User user){
+        logger.info("add开始");
         return userService.addUser(user);
     }
 
