6788eaa647a2b360673723bffe65d91d4aa2b408
mbyy <17600733846@163.com>
2018-08-07 20:41:42 +0800

    上一次数据比较老的问题已解决，是因为另外一个数据库有同名表的原因

--- src/main/java/com/mbxx/pojo/User.java
+++ src/main/java/com/mbxx/pojo/User.java
@@ -5,47 +5,27 @@ import java.util.Date;
 public class User {
     private Long userId;
 
+    private Long toId;
+
     private String username;
 
     private String password;
-
-    private String nickname;
 
     private Date birthday;
 
     private Integer sex;
 
-    private Integer enabled;
+    private Integer loggin;
 
     private Integer versionNumber;
 
     private Date createDate;
 
-    private Long createBy;
+    private String blog;
 
-    private Long updateBy;
+    private String occupation;
 
-    private Date updateDate;
-
-    private String attribute1;
-
-    private String attribute2;
-
-    private String attribute3;
-
-    private String attribute4;
-
-    private String attribute5;
-
-    private String attribute6;
-
-    private String attribute7;
-
-    private String attribute8;
-
-    private String attribute9;
-
-    private String attribute10;
+    private String gotUrl;
 
     public Long getUserId() {
         return userId;
@@ -53,6 +33,14 @@ public class User {
 
     public void setUserId(Long userId) {
         this.userId = userId;
+    }
+
+    public Long getToId() {
+        return toId;
+    }
+
+    public void setToId(Long toId) {
+        this.toId = toId;
     }
 
     public String getUsername() {
@@ -71,14 +59,6 @@ public class User {
         this.password = password == null ? null : password.trim();
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname == null ? null : nickname.trim();
-    }
-
     public Date getBirthday() {
         return birthday;
     }
@@ -95,12 +75,12 @@ public class User {
         this.sex = sex;
     }
 
-    public Integer getEnabled() {
-        return enabled;
+    public Integer getLoggin() {
+        return loggin;
     }
 
-    public void setEnabled(Integer enabled) {
-        this.enabled = enabled;
+    public void setLoggin(Integer loggin) {
+        this.loggin = loggin;
     }
 
     public Integer getVersionNumber() {
@@ -119,107 +99,27 @@ public class User {
         this.createDate = createDate;
     }
 
-    public Long getCreateBy() {
-        return createBy;
+    public String getBlog() {
+        return blog;
     }
 
-    public void setCreateBy(Long createBy) {
-        this.createBy = createBy;
+    public void setBlog(String blog) {
+        this.blog = blog == null ? null : blog.trim();
     }
 
-    public Long getUpdateBy() {
-        return updateBy;
+    public String getOccupation() {
+        return occupation;
     }
 
-    public void setUpdateBy(Long updateBy) {
-        this.updateBy = updateBy;
+    public void setOccupation(String occupation) {
+        this.occupation = occupation == null ? null : occupation.trim();
     }
 
-    public Date getUpdateDate() {
-        return updateDate;
+    public String getGotUrl() {
+        return gotUrl;
     }
 
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public String getAttribute1() {
-        return attribute1;
-    }
-
-    public void setAttribute1(String attribute1) {
-        this.attribute1 = attribute1 == null ? null : attribute1.trim();
-    }
-
-    public String getAttribute2() {
-        return attribute2;
-    }
-
-    public void setAttribute2(String attribute2) {
-        this.attribute2 = attribute2 == null ? null : attribute2.trim();
-    }
-
-    public String getAttribute3() {
-        return attribute3;
-    }
-
-    public void setAttribute3(String attribute3) {
-        this.attribute3 = attribute3 == null ? null : attribute3.trim();
-    }
-
-    public String getAttribute4() {
-        return attribute4;
-    }
-
-    public void setAttribute4(String attribute4) {
-        this.attribute4 = attribute4 == null ? null : attribute4.trim();
-    }
-
-    public String getAttribute5() {
-        return attribute5;
-    }
-
-    public void setAttribute5(String attribute5) {
-        this.attribute5 = attribute5 == null ? null : attribute5.trim();
-    }
-
-    public String getAttribute6() {
-        return attribute6;
-    }
-
-    public void setAttribute6(String attribute6) {
-        this.attribute6 = attribute6 == null ? null : attribute6.trim();
-    }
-
-    public String getAttribute7() {
-        return attribute7;
-    }
-
-    public void setAttribute7(String attribute7) {
-        this.attribute7 = attribute7 == null ? null : attribute7.trim();
-    }
-
-    public String getAttribute8() {
-        return attribute8;
-    }
-
-    public void setAttribute8(String attribute8) {
-        this.attribute8 = attribute8 == null ? null : attribute8.trim();
-    }
-
-    public String getAttribute9() {
-        return attribute9;
-    }
-
-    public void setAttribute9(String attribute9) {
-        this.attribute9 = attribute9 == null ? null : attribute9.trim();
-    }
-
-    public String getAttribute10() {
-        return attribute10;
-    }
-
-    public void setAttribute10(String attribute10) {
-        this.attribute10 = attribute10 == null ? null : attribute10.trim();
+    public void setGotUrl(String gotUrl) {
+        this.gotUrl = gotUrl == null ? null : gotUrl.trim();
     }
 }
